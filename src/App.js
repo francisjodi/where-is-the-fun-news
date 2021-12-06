4ec43549d6b16cb3e92ec93cecf68df3a1d782f2
Jodi-Ann Francis <53411733+francisjodi@users.noreply.github.com>
2021-12-05 20:22:45 -0800

    api attemp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { createContext, useEffect, useState } from "react";
+import axios from "axios";
 import { NewsContextProvider } from "./NewsContext";
 import News from "./components/News";
-import "./app.css";
-
 import "./app.css";
 
 function App() {
