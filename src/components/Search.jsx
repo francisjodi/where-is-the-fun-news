c323162a7499aba54ad47eb006112314e32b44a0
ianyimi <anyiminene@gmail.com>
2021-12-09 19:31:38 -0800

    search input css fix

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -12,15 +12,16 @@ const SearchDiv = styled.div`
 `;
 
 const Background = styled.div`
-  padding: 5px;
+  padding: 3px;
   background: rgb(156,45,253);
   background: linear-gradient(0deg, rgba(156,45,253,1) 0%, rgba(34,193,195,1) 100%);
-  border-radius: 3px;
+  border-radius: 20px;
 `;
 
 const Input = styled.input`
-  border-radius: 3px;
+  border-radius: 20px;
   border: none;
+  padding-left: 10px;
 `;
 
 export default function Search() {
